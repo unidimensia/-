e8ba224964542bb50593e955bcd7216000904a67
unidimensia <unidimensia@gmail.com>
2024-09-16 15:48:24 +0530

    s

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function funcho(){
-   alert("Hey you are not registered with us so please signup to enter Unidimensia it will just take little time also by signing up you will get subscription of our newsletter or your are using wrong credentials please check or try again later maybe out server are down")
+   alert("Hey you are not registered with us so please signup to enter Unidimensia it will just take little time also by signing up you will get subscription of our newsletter or your are using wrong credentials please check or try again later maybe our server are down")
 }
 
 
